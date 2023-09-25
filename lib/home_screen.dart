08ebee3212f1bb0ae8025c0203914056cbbf5f9d
lPatricio_VargasR <1722110158@utectulancingo.edu.mx>
2023-09-25 07:54:52 -0600

    Primer Commit

---
+++ lib/home_screen.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+class LandingPage extends StatefulWidget {
+  LandingPage({super.key, required this.userEmail});
+
+  final String userEmail;
+
+  @override
+  State<LandingPage> createState() => _LandingPageState();
+}
+
+class _LandingPageState extends State<LandingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+          appBar: AppBar(
+            title: const Text("Lading Page",
+              style: TextStyle(color: Colors.yellowAccent)),
+            centerTitle: true,
+            backgroundColor: Colors.redAccent,
+          ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          const SizedBox(
+            height: 50,
+          ),
+          const Center(
+            child: Text(
+              "Landing Page",
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                color: Colors.redAccent),
+            )),
+          const SizedBox(
+            height: 50,
+          ),
+          Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Center(
+              child: Text(
+                "Welcome ${widget.userEmail}",
+                style: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.redAccent),
+              ),
+              )
+          ),
+          const SizedBox(
+            height: 50,
+          ),
+          ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text("Logout"),
+          ),
+        ],
+      )
+    );
+  }
+}
+
---
+++ lib/main.dart
@@ -0,0 +1,134 @@
+import 'package:flutter/material.dart';
+
+import 'home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      darkTheme: ThemeData.dark(),
+      home:const HomePage() ,
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  // Llave global
+  // final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Login Page",
+              style: TextStyle(color: Colors.yellowAccent)),
+        centerTitle: true,
+        backgroundColor: Colors.redAccent,
+      ),
+      body: Form(
+        key: _formKey,
+        child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          const SizedBox(
+            height: 50,
+          ),
+          const Center(child: Text("Login Page", style: TextStyle(
+            fontSize: 30,
+            fontWeight: FontWeight.bold,
+            color: Colors.redAccent),
+          )),
+         const SizedBox(
+            height: 50,
+          ),
+          Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                // Método para validar formulario
+                onFieldSubmitted: (value){
+                  print(value);
+                },
+                validator: (value){
+                  if(value!.isEmpty||
+                      !RegExp(
+                          r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                      .hasMatch(value)){
+                    return "Please enter your email";
+                  }
+                  return null;
+                },
+                controller: emailController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                  ),
+                  hintText: "Enter your email",
+                  labelText: "Email"),
+              ),
+          ),
+          const SizedBox(
+            height: 50,
+          ),
+            Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextFormField(
+              obscureText: true,
+              controller: passwordController,
+              decoration: const InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                  ),
+                  hintText: "Enter your password",
+                  labelText: "Password"),
+            ),
+          ),
+          const SizedBox(
+            height: 50,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 50.0, right: 50.0),
+          child: ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => LandingPage(
+                        userEmail: emailController.text.trim(),
+                      )),
+                ).then((res){
+                  emailController.clear();
+                  passwordController.clear();
+                }); // Proceso futuro que pasará después de la ejecución de otro
+                // emailController.clear();
+                // passwordController.clear();
+              },
+              child: const Text(
+                "Login",
+                style: TextStyle(fontSize: 20),
+              )),
+          )
+        ],
+        )
+      ),
+    );
+  }
+}
